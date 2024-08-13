266967c8c3143bab8724297ff87b2c942f027136
hhh <milosz.mazurek@gmail.com>
2024-08-13 15:48:30 +0200

    Corrected coins change because of the bug

--- src/main/java/dynamicprogramming/other/CoinChange.java
+++ src/main/java/dynamicprogramming/other/CoinChange.java
@@ -6,25 +6,38 @@ import java.util.Arrays;
 /* Leet code 322. Coin Change
  *  Fajne wytlumaczenie https://www.codesdope.com/course/algorithms-coin-change/
  *  https://www.codesdope.com/course/algorithms-coin-change/
+ *
+ * Tutaj na udemy moze byc fajny tutorial do dynamic programing i nie tylko. Ale jeszcze nie przegladalem tego tutoriala
+ * Recursion, Backtracking and Dynamic Programming in Java
+ * Created by Holczel Balazs
+ * https://www.udemy.com/course/algorithmic-problems-in-java/?utm_source=adwords&utm_medium=udemyads&utm_campaign=Search_Keyword_Beta_Prof_la.EN_cc.ROW-English&campaigntype=Search&portfolio=ROW-English&language=EN&product=Course&test=&audience=Keyword&topic=Dynamic_Programming&priority=Beta&utm_content=deal4584&utm_term=_._ag_159583869254_._ad_696298538858_._kw_dynamic+programming+tutorial_._de_c_._dm__._pl__._ti_aud-2268488108799%3Akwd-566707495161_._li_9067410_._pd__._&matchtype=b&gad_source=1&gclid=Cj0KCQjwiOy1BhDCARIsADGvQnCepAHglZd9aNS6XdcYqoma85zL73LCmpfKm_AENcUYWBbNVHMLCcIaAlc_EALw_wcB&couponCode=2021PM20
  * */
 public class CoinChange {
 
     //bottom up dynamic programming
     public int coinChangeBottomUp(int[] coins, int amount) {
-        int min = Integer.MAX_VALUE;
-        int[] changes = new int[amount + 1];//Initial array elements are initialized to default vars. https://stackoverflow.com/questions/3426843/what-is-the-default-initialization-of-an-array-in-java
+        int[] changes = new int[amount + 1];
+        for (int i = 0; i< changes.length;i++) {
+            changes[i] = Integer.MAX_VALUE;
+        }
+
 
         changes[0] = 0;
         for (int j = 1; j <= amount; j++) {
-            for (int i = 0; i < coins.length; i++) {
-                if (coins[i] <= j) {
-                    changes[j] = Integer.min(min, 1 + changes[j - coins[i]]);
+            for (int i = 0; i < coins.length; i++) { // wewnetrzna iteracja jest po tablicy coin-ow. Sprawdzamy wszystkie coiny po kolei ktory jest najlepszy do wymiany zaluzmy ze mamy [1,2,3,10] i amount = 3 i sprawdzamy aktualnie dla coin-a 3 centy
+                if (coins[i] <= j) { // jesli coint z tabelki coin-ow ma wartosc wieksza niz amount to nie nadaje sie do wymiany bo przekracza nasza kwote
+                    changes[j] = Integer.min(changes[j], 1 + changes[j - coins[i]]); //changes[3] = min(MAX_VALUE, 1+ changes[3 - coins[2]]) = min(MAX_VALUE, 1 + changes[3-3])
                 }
             }
         }
         return changes[amount];
     }
 
+
+    /*
+    * Wytlumaczenie top down dosc dobre
+    * https://betterprogramming.pub/learn-dynamic-programming-the-coin-change-problem-22a104478f50
+    * */
     public int coinChangeTopDownMain(int[] coins, int amount) {
         int[] changes = new int[amount + 1];
         Arrays.fill(changes, Integer.MAX_VALUE);
@@ -33,20 +46,21 @@ public class CoinChange {
 
     //top down dynamic programming
     public int coinChangeTopDown(int[] coins, int amount, int[] changes) {
-        if (amount == 0) {
+        if (amount == 0) { // dla rozmiany amount 0 potrzemujemy 0 monet jednoczesnie oznacza to ze moneta rozmieniajaca byla trafna bo zeszla nam z wartoscia do 0 i w linii *** wyjdzie na z rekurencji zwracajac
+                           //  Integer.min(min, 1 + coinChangeTopDown(coins, amount - coins[i], changes));  = Integer.min(Integer.MAX_VALUE, 1 + 0) = 1. Ten przyklad dla 3 centow z linii (&&&&&&) fajnie sobie przegladnac
             return 0;
         }
-        if(changes[amount] != Integer.MAX_VALUE) {
+        if(changes[amount] != Integer.MAX_VALUE) { // jesli juz wyliczylismy w ktorejs z poprzednich glebszych rekursji to nie wyliczamy jeszcze raz tylko reuzywamy
             return changes[amount];
         }
 
         int min = Integer.MAX_VALUE;
-        for (int i = 0; i < coins.length; i++) {
+        for (int i = 0; i < coins.length; i++) { // (&&&&&&) zaluzmy ze amount zostalo 3 i mamy monety [1,2,3,10] jak dojdzie do monety 3 centy to Integer.min(min, 1 + coinChangeTopDown(coins, amount - coins[i], changes)); zwroci 1, dla 1, 2 wyzsze wartosci a do 10 nie dojdzie bo if (coins[i] <= amount) nie pusci
             if (coins[i] <= amount) {
-                min = Integer.min(min, 1 + coinChangeTopDown(coins, amount - coins[i], changes));
+                min = Integer.min(min, 1 + coinChangeTopDown(coins, amount - coins[i], changes)); // (***)
             }
         }
-        changes[amount] = min;
+        changes[amount] = min; // zapamietujemy wyliczana min ilosc rozmien do reuzycia
 
         return min;
     }
--- src/test/java/dynamicprogramming/other/CoinChangeTest.java
+++ src/test/java/dynamicprogramming/other/CoinChangeTest.java
@@ -27,6 +27,29 @@ class CoinChangeTest {
         assertEquals(excpectedChanges, actualChanges);
     }
 
+
+    @ParameterizedTest(name = "{index} --> amount={0} excpectedChanges={1}")
+    @CsvSource({
+            "2, 1"
+    })
+    void shouldReturnCorrectNumChangesBottomUp2(int amount, int excpectedChanges) {
+        int[] coins = {2,1,3,10};
+        int actualChanges = coinChange.coinChangeBottomUp(coins, amount);
+        assertEquals(excpectedChanges, actualChanges);
+    }
+
+    @ParameterizedTest(name = "{index} --> amount={0} excpectedChanges={1}")
+    @CsvSource({
+            "19, 3",
+            "11, 2"
+    })
+    void shouldReturnCorrectNumChangesBottomUp3(int amount, int excpectedChanges) {
+        int[] coins = {9, 6, 5, 1};
+        int actualChanges = coinChange.coinChangeBottomUp(coins, amount);
+        assertEquals(excpectedChanges, actualChanges);
+    }
+
+
     @ParameterizedTest(name = "{index} --> amount={0} excpectedChanges={1}")
     @CsvSource({
             "6, 2",
